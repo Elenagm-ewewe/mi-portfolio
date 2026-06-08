d2407c43f8b5ed58e87fa81cd9879a505db5a2c3
Elenagm-ewewe <egutmor699@g.educaand.es>
2026-06-08 16:56:41 +0200

    creacion de secciones y planteamiento inicial

---
+++ src/content/content.ts
@@ -0,0 +1,23 @@
+export const contacto = {
+    email: "elenapapoula@gmail.com",
+    linkedin: "https://www.linkedin.com/in/elenapapoula/",
+    github: "https://github.com/elenapapoula"
+}
+
+export const proyectos = [
+    {
+        title: "Proyecto 1",
+        description: "Descripción del proyecto 1",
+        link: ""
+    },
+    {
+        title: "Proyecto 2",
+        description: "Descripción del proyecto 2",  
+        link: ""
+    },
+    {
+        title: "Proyecto 3",
+        description: "Descripción del proyecto 3",
+        link: ""
+    }
+]
